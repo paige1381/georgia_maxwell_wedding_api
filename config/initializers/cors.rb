d8dfe58991d5d19eec1b8581974583c1ce0802ef
Paige Boyer <paige1381@gmail.com>
2018-01-29 10:26:42 -0500

    adjust cors file again

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'https://paige1381.github.io/georgia-maxwell-wedding/public/index.html'
+    origins 'localhost:3001', 'https://paige1381.github.io'
 
     resource '*',
       headers: :any,
